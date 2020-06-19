fd446512375618f1a233fa8dec369a7bb0de712f
DELL <DELL@192.168.0.6>
2020-06-19 12:30:33 +0530

    firstCommit

---
+++ src/main/java/org/oar/app/Hen/App.java
@@ -0,0 +1,13 @@
+package org.oar.app.Hen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
